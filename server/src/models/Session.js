6a566362f32c96dba53180369137d2728ae24137
Chamika <e21415@eng.pdn.ac.lk>
2026-09-18 15:52:28 +0530

    Delete server directory

--- server/src/models/Session.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const schema = new mongoose.Schema({
-  tokenHash: { type: String, required: true, unique: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  expiresAt: { type: Date, required: true, index: { expires: 0 } },
-}, { bufferCommands: false });
-export const Session = mongoose.model('Session', schema);
